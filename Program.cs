a0334fcd50be429c7d94979ba54eaa725565f801
ronitshinde <rony.shinde753@gmail.com>
2023-01-04 16:54:29 +0530

    Added Welcome message

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LineComparisonOOPS
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to line Comparison Computation Program");
+        }
+    }
+}
+
+
+
+
+
