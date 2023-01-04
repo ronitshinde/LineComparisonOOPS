596d48f2f8e75330b460561b15e4bff9248cd804
ronitshinde <rony.shinde753@gmail.com>
2023-01-04 17:04:12 +0530

    Added code to calculate line length

---
+++ CalculateLength.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace LineComparisonOOPS
+{
+    public class CalculateLength
+    {
+        public static void LengthOfLine()
+        {
+            int X1, Y1, X2, Y2;
+
+            Console.WriteLine("Give value of X1");
+            X1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give value of X2");
+            Y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give value of Y1");
+            X2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give value of Y2");
+            Y2 = Convert.ToInt32(Console.ReadLine());
+
+            Double a = (Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
+            Console.WriteLine("Value after squaring and adding : " + a);
+
+            Double Line1 = Math.Sqrt(a);
+            Console.WriteLine("Length of the line : " + Line1);
+
+        }
+    }
+}
+
+        
